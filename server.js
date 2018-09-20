c6939419817098e03dbd9b20b4ae186de05b046c
Kevin <nguyenk215@yahoo.com>
2018-09-20 09:45:50 -0700

    adds projects page

--- server.js
+++ server.js
@@ -34,6 +34,13 @@ hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Here are the products!'
+  });
+});
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
